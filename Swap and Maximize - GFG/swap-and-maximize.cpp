7b73300e4a8ff15c0111146731dde0685782e32f
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2023-07-19 15:12:08 +0530

    Added solution - LeetHub

---
+++ Swap and Maximize - GFG/swap-and-maximize.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+long long int maxSum(int arr[], int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;++i)
+			cin>>arr[i];
+		cout<<maxSum(arr,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+long long int maxSum(int arr[], int n)
+{
+    int ans = 0;
+    sort(arr,arr+n);
+    for(int i = 0; i < n/2; i++){
+       ans -= (2*arr[i]);
+       ans += (2*arr[n-i-1]);
+    }
+    return ans;
+}
