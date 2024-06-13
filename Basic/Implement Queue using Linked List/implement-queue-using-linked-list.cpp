638db97c6c32ecc108f5bbf2177a0d37418d7c0e
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2024-06-13 23:27:37 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement Queue using Linked List/implement-queue-using-linked-list.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+    QueueNode *newNode = new QueueNode(x);
+    if(rear == nullptr){
+        front = rear = newNode;
+        return;
+    }
+    rear->next = newNode;
+    rear = newNode;
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+    if(front == nullptr){
+        return -1;
+    }       
+    int result = front->data;
+    QueueNode *temp = front;
+    front = front->next;
+    if(front == nullptr){
+        rear = nullptr;
+    }
+    delete temp;
+    return result;
+}
