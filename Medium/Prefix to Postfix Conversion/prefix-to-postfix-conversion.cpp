d2909e68821a9fd819fddbb100069a1f24078875
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2024-06-15 12:33:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  private:
+    bool isOperator(char c) {
+        return (c == '+' || c == '-' || c == '*' || c == '/');
+    }
+  public:
+    string preToPost(string pre_exp) {
+        stack<string> st;
+        int length = pre_exp.size();
+        for(int i=length-1;i>=0;i--){
+            if(isOperator(pre_exp[i])){
+                string op1 = st.top();
+                st.pop();
+                string op2 = st.top();
+                st.pop();
+                string temp = op1 + op2 + pre_exp[i];
+                st.push(temp);
+            }
+            else{
+                st.push(string(1, pre_exp[i]));
+            }
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string prefix;
+        cin >> prefix;
+
+        Solution obj;
+        cout << obj.preToPost(prefix) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
