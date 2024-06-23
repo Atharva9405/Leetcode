26014c157003d3739dd78a851ef8786810233695
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2024-06-23 11:35:16 +0530

    Time: 330 ms (77.08%), Space: 168.8 MB (72.46%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,76 @@
+class LRUCache {
+private:
+    struct Node {
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+
+    int capacity;
+    Node* head;
+    Node* tail;
+    std::unordered_map<int, Node*> cache;
+
+    void addToFront(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToFront(Node* node) {
+        removeNode(node);
+        addToFront(node);
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity) {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            moveToFront(node);
+            return node->value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            moveToFront(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node* lru = tail->prev;
+                removeNode(lru);
+                cache.erase(lru->key);
+                delete lru;
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addToFront(newNode);
+        }
+    }
+
+    ~LRUCache() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+};
