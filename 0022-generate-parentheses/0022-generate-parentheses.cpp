343ec89e8510ee20a878b43c0706afd52e43bb2e
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2024-07-07 12:08:09 +0530

    Time: 9 ms (11.98%), Space: 12 MB (80.33%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> res;
+    bool isValid(string& str){
+        int count = 0;
+        for(char&ch: str){
+            if(ch == '('){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count<0){
+                return false;
+            }
+        }
+        return count==0;
+
+    }
+
+    void solve(string& curr,int n){
+        if(curr.length() == 2*n){
+            if(isValid(curr)){
+                res.push_back(curr);
+            }
+            return;
+        }
+        curr.push_back('(');
+        solve(curr,n);
+        curr.pop_back();
+        curr.push_back(')');
+        solve(curr,n);
+        curr.pop_back();
+    }
+
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        solve(curr,n);
+        return res;
+    }
+};
