879e73a8314c79b26caa0a80a5ae1ebbba66b48b
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2024-07-20 11:52:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void dfs(int node,vector<int> adjLs[],int vis[]){
+        vis[node] = 1;
+        for(auto it: adjLs[node]){
+            if(!vis[it]){
+                dfs(it,adjLs,vis);
+            }
+        }
+    }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        vector<int> adjLs[V];
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(adj[i][j] == 1 && i!=j){
+                    adjLs[i].push_back(j);
+                    adjLs[j].push_back(i);
+                }
+            }
+        }
+        int vis[V] = {0};
+        int count=0;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                count++;
+                dfs(i,adjLs,vis);
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
