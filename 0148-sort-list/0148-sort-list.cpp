d6f7a205891a55abefc3c438b8fcc5f7f5923ae2
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2025-01-04 23:42:54 +0530

    Time: 7 ms (96.91%), Space: 56.9 MB (86.26%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -8,23 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+    ListNode* getMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int>ans;
-        ListNode *temp=head;
-        while(temp){
-            ans.push_back(temp->val);
-            temp=temp->next;
+        if (!head || !head->next) {
+            return head;
         }
-        sort(ans.begin(),ans.end());
-        int i=0;
-        temp=head;
-        while(temp){
-            temp->val=ans[i];
-            temp=temp->next;
-            i++;
-        }
-        return head;
+
+        ListNode* middle = getMiddle(head);
+        ListNode* secondHalf = middle->next;
+        middle->next = nullptr;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(secondHalf);
+
+        return merge(left, right);
     }
 };
