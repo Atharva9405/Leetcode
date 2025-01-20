31580827a57ddbe368863f71f7287b4805d1912e
Atharva Nagore <96339246+Atharva9405@users.noreply.github.com>
2025-01-21 01:09:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print linked list */
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Node is defined as
+    struct node
+    {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+
+    }*head;
+*/
+
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+        vector<Node *>arr;
+        while(head)
+        {
+            arr.push_back(head);
+            head=head->next;
+        }
+        int n=arr.size(),s=0;
+        while(s<arr.size())
+        {
+            reverse(arr.begin()+s,arr.begin()+min(s+k,n));
+            s+= k;
+        }
+        head=arr[0];
+        Node *temp=head;
+        for(int i=1;i<n;++i)
+        {
+            temp->next=arr[i];
+            temp= temp->next;
+        }
+        temp->next=nullptr;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        Node* head = new Node(data);
+        Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+
+        Solution ob;
+        head = ob.reverseKGroup(head, k);
+        printList(head);
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
